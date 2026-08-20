da80ea89b6fe4ac1ed31424c8b6c6ab236caef56
Ziad <z7yt9730@gmail.com>
2026-08-20 10:04:49 +0300

    added the classification data sets & added the metric: 'accuracy'

--- utils/metrics.py
+++ utils/metrics.py
@@ -8,3 +8,6 @@ def r2(y: np.ndarray, y_pred: np.ndarray) -> float:
     y_mean = np.mean(y)
 
     return 1 - (np.mean((y - y_pred)**2))/(np.mean((y - y_mean)**2))
+
+def accuracy(y: np.ndarray, y_pred_class: np.ndarray) -> float:
+    return np.mean(y == y_pred_class)
